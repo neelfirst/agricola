2a558ff82290a7ed47ee6ca67fa9d1eb61862cda
neelfirst <neelfirst@gmail.com>
2020-11-25 22:59:23 -0800

    tabs to spaces

--- scripts/parser.py
+++ scripts/parser.py
@@ -5,14 +5,14 @@ import urllib.request, urllib.error, urllib.parse, os
 from bs4 import BeautifulSoup
 
 def getBonus(bonus):
-	X = bonus.split(' => ')
-	b = 0
-	for x in X[1:]:
-		b += int(x[0])
-	return b	
+    X = bonus.split(' => ')
+    b = 0
+    for x in X[1:]:
+        b += int(x[0])
+    return b    
 
 def getPlayer(NT,cols):
-	return {'player' : NT[0] , 'fields' : int(cols[0]) , 'pastures' : int(cols[1]) , 'grain':int(cols[2]) , \
+    return {'player' : NT[0] , 'fields' : int(cols[0]) , 'pastures' : int(cols[1]) , 'grain':int(cols[2]) , \
 'vegetables' : int(cols[3]) , 'sheep' : int(cols[4]) , 'boar' : int(cols[5]) , 'cattle' : int(cols[6]), \
 'unused' : int(cols[7]) , 'fstables' : int(cols[8]) , 'rooms' : int(cols[9]) , 'family':int(cols[10]), \
 'cards':int(cols[11]),'bonus':getBonus(cols[12]),'total':int(NT[1])}
@@ -33,10 +33,10 @@ players = []
 rows = scoreTable.find_all('tr')
 i = 0
 for row in rows:
-	cols = row.find_all('td')
-	cols = [ele.text.strip() for ele in cols]
-	if cols != []:
-		players.append(getPlayer(playerTotals[i:i+2],cols))
-		i += 2
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    if cols != []:
+        players.append(getPlayer(playerTotals[i:i+2],cols))
+        i += 2
 
 # final score table in players
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -9,47 +9,45 @@ STOP = 3765006
 STEP = 1
 
 def getPage(url):
-	user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-	headers = { 'User-Agent' : user_agent }
-	req = urllib.request.Request(url, None, headers)
-	response = urllib.request.urlopen(req)
-	page = response.read()
-	response.close()
-	return page
+    user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+    headers = { 'User-Agent' : user_agent }
+    req = urllib.request.Request(url, None, headers)
+    response = urllib.request.urlopen(req)
+    page = response.read()
+    response.close()
+    return page
 
-def checkIfGameIsDone(game,history):
-	isGameOver = "- GAME OVER -" in game
-	hasGivenUp = "gives up!" in history
-	hasLayaboutEffect = "\"Layabout\" effect" in history
-	isRound14 = "<td class=\"clHisto\" style=\"text-align:center;vertical-align:middle;font-weight:bold;color:red\">14</td>" in history
-	numPlayers = history.count("</div>&nbsp")
-	numFeedings = history.count("feeding")
-	if (isRound14 and isGameOver and numPlayers*6 == numFeedings+hasLayaboutEffect):
-		return True
-	else:
-		return False
+def checkIfGameIsDone(history):
+    print(history)
+    hasLayaboutEffect = "\"Layabout\" effect" in history
+    numPlayers = 5
+    numFeedings = history.count("feeding")
+    if (numPlayers*6 == numFeedings + hasLayaboutEffect):
+        return True
+    else:
+        return False
 
 def writePage(url,ext,page):
-	f = open(url[-7:]+ext,'w')
-	f.write(page)
-	f.close()
+    f = open(url[-7:]+ext,'w')
+    f.write(page)
+    f.close()
 
 def main(argv):
-	for i in range(START,STOP,STEP):
-		try:
-			urlPartie = "http://www.boiteajeux.net/jeux/agr/partie.php?id="+str(i)
-			urlHistorique = "http://www.boiteajeux.net/jeux/agr/historique.php?id="+str(i)
-			pageGame = getPage(urlPartie)
-			pageHistory = getPage(urlHistorique)
-			if checkIfGameIsDone(pageGame,pageHistory):
-				writePage(urlPartie,"-game.html",pageGame)
-				writePage(urlHistorique,"-history.html",pageHistory)
-				print("valid game i = ",i)
-		except Exception as e:
-			print(i,e)
-			continue
-		if (i % 5000 == 0):
-			print("checkpoint i = ",i)
+    for i in range(START,STOP,STEP):
+        try:
+            urlPartie = "http://www.boiteajeux.net/jeux/agr/partie.php?id="+str(i)
+            urlHistorique = "http://www.boiteajeux.net/jeux/agr/historique.php?id="+str(i)
+            pageGame = getPage(urlPartie)
+            pageHistory = getPage(urlHistorique)
+            if checkIfGameIsDone(pageGame,pageHistory):
+                writePage(urlPartie,"-game.html",pageGame)
+                writePage(urlHistorique,"-history.html",pageHistory)
+                print("valid game i = ",i)
+        except Exception as e:
+            print(i,e)
+            continue
+        if (i % 5000 == 0):
+            print("checkpoint i = ",i)
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
