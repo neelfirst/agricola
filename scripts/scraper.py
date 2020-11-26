cb818e0f1474c493225625b7748efe725a3809ef
neelfirst <neelfirst@gmail.com>
2020-11-25 22:43:12 -0800

    var names

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -3,37 +3,39 @@
 import sys
 import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
-import numpy as np
 
-def getPage(URL):
+START = 3765004
+STOP = 3765006
+STEP = 1
+
+def getPage(url):
 	user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
 	headers = { 'User-Agent' : user_agent }
-	req = urllib.request.Request(URL, None, headers)
+	req = urllib.request.Request(url, None, headers)
 	response = urllib.request.urlopen(req)
 	page = response.read()
 	response.close()
 	return page
 
-def checkIfGameIsDone(GAME,HISTORY):
-	isGameOver = "- GAME OVER -" in GAME
-	hasGivenUp = "gives up!" in HISTORY
-	hasLayaboutEffect = "\"Layabout\" effect" in HISTORY
-	isRound14 = "<td class=\"clHisto\" style=\"text-align:center;vertical-align:middle;font-weight:bold;color:red\">14</td>" in HISTORY
-	numPlayers = HISTORY.count("</div>&nbsp")
-	numFeedings = HISTORY.count("feeding")
+def checkIfGameIsDone(game,history):
+	isGameOver = "- GAME OVER -" in game
+	hasGivenUp = "gives up!" in history
+	hasLayaboutEffect = "\"Layabout\" effect" in history
+	isRound14 = "<td class=\"clHisto\" style=\"text-align:center;vertical-align:middle;font-weight:bold;color:red\">14</td>" in history
+	numPlayers = history.count("</div>&nbsp")
+	numFeedings = history.count("feeding")
 	if (isRound14 and isGameOver and numPlayers*6 == numFeedings+hasLayaboutEffect):
 		return True
 	else:
 		return False
-	
 
-def writePage(URL,EXT,PAGE):
-	f = open(URL[-7:]+EXT,'w')
-	f.write(PAGE)
+def writePage(url,ext,page):
+	f = open(url[-7:]+ext,'w')
+	f.write(page)
 	f.close()
 
 def main(argv):
-	for i in np.arange(850000,2400000,1):
+	for i in range(START,STOP,STEP):
 		try:
 			urlPartie = "http://www.boiteajeux.net/jeux/agr/partie.php?id="+str(i)
 			urlHistorique = "http://www.boiteajeux.net/jeux/agr/historique.php?id="+str(i)
