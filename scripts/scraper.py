ffd3010d2c9aaba977c0caac16bc34f89c32ca2d
neelfirst <neelfirst@gmail.com>
2020-11-25 23:20:05 -0800

    working n=1 scrape

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,26 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
-import urllib.request, urllib.error, urllib.parse
+import requests
 from bs4 import BeautifulSoup
 
-START = 3765004
-STOP = 3765006
+START = 3525005
+STOP = 3525006
 STEP = 1
 
 def getPage(url):
-    user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-    headers = { 'User-Agent' : user_agent }
-    req = urllib.request.Request(url, None, headers)
-    response = urllib.request.urlopen(req)
-    page = response.read()
-    response.close()
-    return page
+    r = requests.get(url)
+    return r.text
 
 def checkIfGameIsDone(history):
-    print(history)
     hasLayaboutEffect = "\"Layabout\" effect" in history
-    numPlayers = 5
+    numPlayers = history.count("</div>&nbsp")
     numFeedings = history.count("feeding")
     if (numPlayers*6 == numFeedings + hasLayaboutEffect):
         return True
@@ -35,13 +29,13 @@ def writePage(url,ext,page):
 def main(argv):
     for i in range(START,STOP,STEP):
         try:
-            urlPartie = "http://www.boiteajeux.net/jeux/agr/partie.php?id="+str(i)
-            urlHistorique = "http://www.boiteajeux.net/jeux/agr/historique.php?id="+str(i)
-            pageGame = getPage(urlPartie)
-            pageHistory = getPage(urlHistorique)
-            if checkIfGameIsDone(pageGame,pageHistory):
-                writePage(urlPartie,"-game.html",pageGame)
-                writePage(urlHistorique,"-history.html",pageHistory)
+  #          partie = "http://www.boiteajeux.net/jeux/agr/partie.php?id="+str(i)
+            historique = "http://www.boiteajeux.net/jeux/agr/historique.php?id="+str(i)
+  #          game = getPage(partie)
+            history = getPage(historique)
+            if checkIfGameIsDone(history):
+  #              writePage(partie,"-game.html",game)
+                writePage(historique,"-history.html",history)
                 print("valid game i = ",i)
         except Exception as e:
             print(i,e)
